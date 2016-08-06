8535e2ec97b33336041c1f90cc8059c69d13670a
sameraamar <samer.aamar@gmail.com>
2016-08-06 11:36:28 +0300

    Create build_recursive_file_names.py

---
+++ build_recursive_file_names.py
@@ -0,0 +1,31 @@
+from os import walk
+from os.path import split, splitext, join, exists
+
+def select_files(root, files, extensions=[]):
+    """
+    simple logic here to filter out interesting files based on extensions
+    """
+
+    selected_files = []
+
+    for file in files:
+        #do concatenation here to get full path 
+        full_path = join(root, file)
+        ext = splitext(file)[1]
+
+        if ext in extensions:
+            selected_files.append(full_path)
+
+    return selected_files
+
+def build_recursive_dir_tree(path, ext=[]):
+    """
+    path    -    where to begin folder scan
+    """
+    selected_files = []
+
+    for root, dirs, files in walk(path):
+        selected_files += select_files(root, files, extensions=ext)
+
+    return selected_files
+    
